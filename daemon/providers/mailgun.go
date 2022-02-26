34952870f2e7da829bb1e283866470e99bcbb786
Alex Krantz <alex@krantz.dev>
2022-02-26 02:59:12 -0800

    Add mailgun provider

---
+++ daemon/providers/mailgun.go
@@ -0,0 +1,63 @@
+package providers
+
+import (
+	"context"
+	"fmt"
+	"net/mail"
+	"os"
+	"strings"
+
+	"github.com/mailgun/mailgun-go/v4"
+)
+
+type MailGun struct {
+	mg mailgun.Mailgun
+}
+
+func (m *MailGun) Send(ctx context.Context, to mail.Address, from mail.Address, subject string, body string, htmlBody, replyTo *string) error {
+	msg := m.mg.NewMessage(from.String(), subject, body, to.String())
+	if htmlBody != nil {
+		msg.SetHtml(*htmlBody)
+	}
+	if replyTo != nil {
+		msg.SetReplyTo(*replyTo)
+	}
+
+	_, _, err := m.mg.Send(ctx, msg)
+	return err
+}
+
+func (m *MailGun) SendBatch(ctx context.Context, to []mail.Address, from mail.Address, subject string, body string, htmlBody, replyTo *string) error {
+	msg := m.mg.NewMessage(from.String(), subject, body)
+	for _, address := range to {
+		if err := msg.AddRecipient(address.String()); err != nil {
+			return err
+		}
+	}
+	if htmlBody != nil {
+		msg.SetHtml(*htmlBody)
+	}
+	if replyTo != nil {
+		msg.SetReplyTo(*replyTo)
+	}
+
+	_, _, err := m.mg.Send(ctx, msg)
+	return err
+}
+
+// NewMailgun creates a new MailGun email provider
+func NewMailgun(id string) (Provider, error) {
+	envId := strings.ToUpper(id)
+	apiKey := os.Getenv(fmt.Sprintf("MAILER_PROVIDER_%s_API_KEY", envId))
+	if len(apiKey) == 0 {
+		return nil, fmt.Errorf("missing api key for mailgun provider %q", id)
+	}
+	domain := os.Getenv(fmt.Sprintf("MAILER_PROVIDER_%s_DOMAIN", envId))
+	if len(domain) == 0 {
+		return nil, fmt.Errorf("missing domain for mailgun provider %q", id)
+	}
+
+	return &MailGun{
+		mg: mailgun.NewMailgun(domain, apiKey),
+	}, nil
+}
