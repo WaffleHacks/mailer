acb503f411e54643fc0c08016d75c0b5ab7296d7
Alex Krantz <alex@krantz.dev>
2022-02-26 16:54:47 -0800

    Add smtp provider

--- go.mod
+++ go.mod
@@ -40,4 +40,6 @@ require (
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
---
+++ providers/smtp.go
@@ -0,0 +1,90 @@
+package providers
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"go.uber.org/zap"
+	"gopkg.in/gomail.v2"
+)
+
+type SMTP struct {
+	dialer *gomail.Dialer
+	sender gomail.SendCloser
+	open   bool
+}
+
+func (s *SMTP) Send(_ context.Context, l *zap.Logger, to, from, subject, body string, htmlBody, replyTo *string) error {
+	// Reconnect if necessary
+	if !s.open {
+		sender, err := s.dialer.Dial()
+		if err != nil {
+			l.Error("failed to reconnect to smtp provider")
+			return err
+		}
+		l.Info("reconnected to smtp provider")
+		s.sender = sender
+		s.open = true
+	}
+
+	// Construct the message
+	msg := gomail.NewMessage(gomail.SetCharset("UTF-8"))
+	msg.SetHeader("To", to)
+	msg.SetHeader("From", from)
+	msg.SetHeader("Subject", subject)
+	msg.SetBody("text/plain", body)
+	if htmlBody != nil {
+		msg.AddAlternative("text/html", *htmlBody)
+	}
+	if replyTo != nil {
+		msg.SetHeader("Reply-To", *replyTo)
+	}
+
+	// Send the message
+	if err := s.sender.Send(from, []string{to}, msg); err != nil {
+		s.open = false
+		return err
+	}
+
+	return nil
+}
+
+func NewSMTP(id string) (Provider, error) {
+	envId := strings.ToUpper(id)
+	host := os.Getenv(fmt.Sprintf("MAILER_PROVIDER_%s_HOST", envId))
+	if len(host) == 0 {
+		return nil, fmt.Errorf("missing host for smtp provider %q", id)
+	}
+	port, err := strconv.Atoi(os.Getenv(fmt.Sprintf("MAILER_PROVIDER_%s_PORT", envId)))
+	if err != nil {
+		return nil, fmt.Errorf("invalid port for smtp provider %q: %v", id, err)
+	}
+	ssl := strings.ToLower(os.Getenv(fmt.Sprintf("MAILER_PROVIDER_%s_SSL", envId)))
+	username := os.Getenv(fmt.Sprintf("MAILER_PROVIDER_%s_USERNAME", envId))
+	if len(username) == 0 {
+		return nil, fmt.Errorf("missing username for smtp proivder %q", id)
+	}
+	password := os.Getenv(fmt.Sprintf("MAILER_PROVIDER_%s_PASSWORD", envId))
+	if len(password) == 0 {
+		return nil, fmt.Errorf("missing password for smtp provider %q", id)
+	}
+
+	// Create the dialer
+	dialer := gomail.NewDialer(host, port, username, password)
+	dialer.SSL = ssl == "y" || ssl == "yes" || ssl == "t" || ssl == "true"
+
+	// Test the connection
+	sender, err := dialer.Dial()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to smtp provider %q: %v", id, err)
+	}
+
+	return &SMTP{
+		dialer: dialer,
+		sender: sender,
+		open:   true,
+	}, nil
+}
